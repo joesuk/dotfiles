340cab59709693ea3ed0ce579c58770ebe04da07
a <ybjosuk@gmail.com>
2024-09-09 22:25:29 -0400

    install script; no librewolf

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -185,59 +185,59 @@ vimplugininstall() {
 	sudo -u "$name" nvim -c "PlugInstall|q|q"
 }
 
-makeuserjs(){
-	# Get the Arkenfox user.js and prepare it.
-	arkenfox="$pdir/arkenfox.js"
-	overrides="$pdir/user-overrides.js"
-	userjs="$pdir/user.js"
-	ln -fs "/home/$name/.config/firefox/larbs.js" "$overrides"
-	[ ! -f "$arkenfox" ] && curl -sL "https://raw.githubusercontent.com/arkenfox/user.js/main/user.js" > "$arkenfox"
-	cat "$arkenfox" "$overrides" > "$userjs"
-	chown "$name:wheel" "$arkenfox" "$userjs"
-	# Install the updating script.
-	mkdir -p /usr/local/lib /etc/pacman.d/hooks
-	cp "/home/$name/.local/bin/arkenfox-auto-update" /usr/local/lib/
-	chown root:root /usr/local/lib/arkenfox-auto-update
-	chmod 755 /usr/local/lib/arkenfox-auto-update
-	# Trigger the update when needed via a pacman hook.
-	echo "[Trigger]
-Operation = Upgrade
-Type = Package
-Target = firefox
-Target = librewolf
-Target = librewolf-bin
-[Action]
-Description=Update Arkenfox user.js
-When=PostTransaction
-Depends=arkenfox-user.js
-Exec=/usr/local/lib/arkenfox-auto-update" > /etc/pacman.d/hooks/arkenfox.hook
-}
+#makeuserjs(){
+	## Get the Arkenfox user.js and prepare it.
+	#arkenfox="$pdir/arkenfox.js"
+	#overrides="$pdir/user-overrides.js"
+	#userjs="$pdir/user.js"
+	#ln -fs "/home/$name/.config/firefox/larbs.js" "$overrides"
+	#[ ! -f "$arkenfox" ] && curl -sL "https://raw.githubusercontent.com/arkenfox/user.js/main/user.js" > "$arkenfox"
+	#cat "$arkenfox" "$overrides" > "$userjs"
+	#chown "$name:wheel" "$arkenfox" "$userjs"
+	## Install the updating script.
+	#mkdir -p /usr/local/lib /etc/pacman.d/hooks
+	#cp "/home/$name/.local/bin/arkenfox-auto-update" /usr/local/lib/
+	#chown root:root /usr/local/lib/arkenfox-auto-update
+	#chmod 755 /usr/local/lib/arkenfox-auto-update
+	## Trigger the update when needed via a pacman hook.
+	#echo "[Trigger]
+#Operation = Upgrade
+#Type = Package
+#Target = firefox
+#Target = librewolf
+#Target = librewolf-bin
+#[Action]
+#Description=Update Arkenfox user.js
+#When=PostTransaction
+#Depends=arkenfox-user.js
+#Exec=/usr/local/lib/arkenfox-auto-update" > /etc/pacman.d/hooks/arkenfox.hook
+#}
 
-installffaddons(){
-	addonlist="ublock-origin decentraleyes istilldontcareaboutcookies vim-vixen"
-	addontmp="$(mktemp -d)"
-	trap "rm -fr $addontmp" HUP INT QUIT TERM PWR EXIT
-	IFS=' '
-	sudo -u "$name" mkdir -p "$pdir/extensions/"
-	for addon in $addonlist; do
-		if [ "$addon" = "ublock-origin" ]; then
-			addonurl="$(curl -sL https://api.github.com/repos/gorhill/uBlock/releases/latest | grep -E 'browser_download_url.*\.firefox\.xpi' | cut -d '"' -f 4)"
-		else
-			addonurl="$(curl --silent "https://addons.mozilla.org/en-US/firefox/addon/${addon}/" | grep -o 'https://addons.mozilla.org/firefox/downloads/file/[^"]*')"
-		fi
-		file="${addonurl##*/}"
-		sudo -u "$name" curl -LOs "$addonurl" > "$addontmp/$file"
-		id="$(unzip -p "$file" manifest.json | grep "\"id\"")"
-		id="${id%\"*}"
-		id="${id##*\"}"
-		mv "$file" "$pdir/extensions/$id.xpi"
-	done
-	chown -R "$name:$name" "$pdir/extensions"
-	# Fix a Vim Vixen bug with dark mode not fixed on upstream:
-	sudo -u "$name" mkdir -p "$pdir/chrome"
-	[ ! -f  "$pdir/chrome/userContent.css" ] && sudo -u "$name" echo ".vimvixen-console-frame { color-scheme: light !important; }
-#category-more-from-mozilla { display: none !important }" > "$pdir/chrome/userContent.css"
-}
+#installffaddons(){
+	#addonlist="ublock-origin decentraleyes istilldontcareaboutcookies vim-vixen"
+	#addontmp="$(mktemp -d)"
+	#trap "rm -fr $addontmp" HUP INT QUIT TERM PWR EXIT
+	#IFS=' '
+	#sudo -u "$name" mkdir -p "$pdir/extensions/"
+	#for addon in $addonlist; do
+		#if [ "$addon" = "ublock-origin" ]; then
+			#addonurl="$(curl -sL https://api.github.com/repos/gorhill/uBlock/releases/latest | grep -E 'browser_download_url.*\.firefox\.xpi' | cut -d '"' -f 4)"
+		#else
+			#addonurl="$(curl --silent "https://addons.mozilla.org/en-US/firefox/addon/${addon}/" | grep -o 'https://addons.mozilla.org/firefox/downloads/file/[^"]*')"
+		#fi
+		#file="${addonurl##*/}"
+		#sudo -u "$name" curl -LOs "$addonurl" > "$addontmp/$file"
+		#id="$(unzip -p "$file" manifest.json | grep "\"id\"")"
+		#id="${id%\"*}"
+		#id="${id##*\"}"
+		#mv "$file" "$pdir/extensions/$id.xpi"
+	#done
+	#chown -R "$name:$name" "$pdir/extensions"
+	## Fix a Vim Vixen bug with dark mode not fixed on upstream:
+	#sudo -u "$name" mkdir -p "$pdir/chrome"
+	#[ ! -f  "$pdir/chrome/userContent.css" ] && sudo -u "$name" echo ".vimvixen-console-frame { color-scheme: light !important; }
+##category-more-from-mozilla { display: none !important }" > "$pdir/chrome/userContent.css"
+#}
 
 finalize() {
 	whiptail --title "All done!" \
@@ -313,7 +313,7 @@ installationloop
 putgitrepo "$dotfilesrepo" "/home/$name" "$repobranch"
 [ -z "/home/$name/.config/newsboat/urls" ] &&
 	echo "$rssurls" > "/home/$name/.config/newsboat/urls"
-rm -rf "/home/$name/.git/" "/home/$name/README.md" "/home/$name/LICENSE" "/home/$name/FUNDING.yml"
+#rm -rf "/home/$name/.git/" "/home/$name/README.md"
 
 # Install vim plugins if not alread present.
 [ ! -f "/home/$name/.config/nvim/autoload/plug.vim" ] && vimplugininstall
@@ -347,25 +347,25 @@ echo "export \$(dbus-launch)" >/etc/profile.d/dbus.sh
 	Option "Tapping" "on"
 EndSection' >/etc/X11/xorg.conf.d/40-libinput.conf
 
-# All this below to get Librewolf installed with add-ons and non-bad settings.
+# All this below to get Librewolf installed with add-ons and non-bad settings. (commented out for now)
 
-whiptail --infobox "Setting browser privacy settings and add-ons..." 7 60
+#whiptail --infobox "Setting browser privacy settings and add-ons..." 7 60
 
-browserdir="/home/$name/.librewolf"
-profilesini="$browserdir/profiles.ini"
+#browserdir="/home/$name/.librewolf"
+#profilesini="$browserdir/profiles.ini"
 
 # Start librewolf headless so it generates a profile. Then get that profile in a variable.
-sudo -u "$name" librewolf --headless >/dev/null 2>&1 &
-sleep 1
-profile="$(sed -n "/Default=.*.default-default/ s/.*=//p" "$profilesini")"
-pdir="$browserdir/$profile"
+#sudo -u "$name" librewolf --headless >/dev/null 2>&1 &
+#sleep 1
+#profile="$(sed -n "/Default=.*.default-default/ s/.*=//p" "$profilesini")"
+#pdir="$browserdir/$profile"
 
-[ -d "$pdir" ] && makeuserjs
+#[ -d "$pdir" ] && makeuserjs
 
-[ -d "$pdir" ] && installffaddons
+#[ -d "$pdir" ] && installffaddons
 
-# Kill the now unnecessary librewolf instance.
-pkill -u "$name" librewolf
+## Kill the now unnecessary librewolf instance.
+#pkill -u "$name" librewolf
 
 # Allow wheel users to sudo with password and allow several system commands
 # (like `shutdown` to run without password).
