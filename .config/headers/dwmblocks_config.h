4872439e2d85b0b8f15e9cca969fa45ce231b7d2
joesuk <ybjosuk@gmail.com>
2024-11-21 11:34:12 +0900

    change update frequency of finance scripts

--- .config/headers/dwmblocks_config.h
+++ .config/headers/dwmblocks_config.h
@@ -15,10 +15,10 @@ static const Block blocks[] = {
 	 /*{"",	"sb-price btc Bitcoin 💰",				9000,	21},*/
 	 /*{"",	"sb-days",				9000,	21},*/
 	 /*{"",	"sb-crypto",				9000,	21},*/
-	 /*{"",	"sb-vtsax",				9000,	21},*/
+	 /*{"",	"sb-finance",				80000,	21},*/
 	/*{"",	"sb-weather",	18000,	5},*/
 	/*{"",	"sb-torrent",	20,	7},*/
-	  {"",	"sb-days",	1,	16},
+	  {"",	"sb-days",	80000,	16},
 	  {"",	"sb-nettraf",	1,	16},
 	 {"",	"sb-memory",	10,	14},
 	 {"",	"sb-cpu",		10,	18},
