0f0b9117ad29808ee437530ad5b97539e97acdec
joesuk <ybjosuk@gmail.com>
2025-02-18 18:49:15 -0500

    LI benq monitor xsetup

---
+++ .local/bin/benq.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 2256x1504 --pos 2880x0 --rotate normal --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --scale 1.5x1.5 --output DP-2 --off --output DP-3 --off --output DP-4 --off &
+setbg
