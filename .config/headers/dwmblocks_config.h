a1fb4402585b7e34f5ac1c1f49c3a87edd7d8b91
joesuk <ybjosuk@gmail.com>
2025-11-26 14:25:28 -0500

    put sbvolume back into dwmblocks

--- .config/headers/dwmblocks_config.h
+++ .config/headers/dwmblocks_config.h
@@ -17,7 +17,7 @@ static const Block blocks[] = {
 	 /*{"",	"sb-finance",				80000,	21},*/
 	/*{"",	"sb-weather",	18000,	5},*/
 	/*{"",	"sb-torrent",	20,	7},*/
-	  /*{"",	"sb-days",	80000,	16},*/
+	  {"",	"sb-days",	40000,	16},
 	  {"",	"sb-nettraf",	1,	16},
 	 {"",	"sb-memory",	10,	14},
 	 {"",	"sb-cpu",		10,	18},
