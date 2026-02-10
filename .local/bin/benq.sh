f916771324dc17484c820ef58bb9aa0d6b728426
joesuk <ybjosuk@gmail.com>
2026-02-10 13:31:18 -0500

    no pywal

--- .local/bin/benq.sh
+++ .local/bin/benq.sh
@@ -4,13 +4,15 @@ if [ ! -e $TOGGLE ]; then
     touch $TOGGLE
     xrandr \
   --output eDP-1 --primary --mode 2256x1504 --pos 0x0 --rotate normal \
-  --output DP-1  --mode 1920x1080 --pos 2880x0 --rotate normal --scale 1.5x1.5 \
+  --output DP-3  --mode 1920x1080 --pos 2880x0 --rotate normal --scale 1.5x1.5 \
+  # ^ switch DP-3 to DP-1 depending on where hdmi port is, see arandr
   --output DP-2 --off --output DP-3 --off --output DP-4 --off
-	setbg && notify-send "2nd monitor enabled"
+	#setbg && notify-send "2nd monitor enabled"
+	notify-send "2nd monitor enabled"
 else
     rm $TOGGLE
     xrandr --output eDP-1 --primary --mode 2256x1504 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off &
-    setbg && notify-send "2nd monitor disabled"
+    notify-send "2nd monitor disabled"
 fi
 #xrandr --output eDP-1 --primary --mode 2256x1504 --pos 2880x0 --rotate normal --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --scale 1.5x1.5 --output DP-2 --off --output DP-3 --off --output DP-4 --off &
 #setbg
